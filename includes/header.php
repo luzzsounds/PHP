f5eb4a138ea89b95460ef79ba2a920f256821901
Ludwig <leb.ludwig@gmail.com>
2018-04-04 14:39:35 +0200

    ecriture menu header

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,10 @@
-<h1>HEADER</h1>
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.php?page=home">Accueil</a></li>
+      <li><a href="index.php?page=registration">Inscription</a></li>
+      <li><a href="index.php?page=contact">Contact</a></li>
+      <li><a href="index.php?page=news">News</a></li>
+      </ul>
+  </nav>
+</header>
