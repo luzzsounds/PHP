92af798781b4f53c08bf838066f64364ac9bd5a7
Ludwig <leb.ludwig@gmail.com>
2018-04-04 14:27:35 +0200

    ajout abbr

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-<p>&copy; <?php echo date ('Y') ?> - Dieppe Agglo Merguez Corp.</p>
+<p>&copy; <?php echo date ('Y') ?> - Dieppe Agglo Merguez <abbr title="Corporation"> Corp.<abbr></p>
 </footer>
