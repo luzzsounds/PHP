042dada682877fbe7b3bad319b51977130a482a3
Ludwig <leb.ludwig@gmail.com>
2018-04-05 09:28:58 +0200

    CREATION FORMULAIRE

---
+++ includes/frmRegistration.php
@@ -0,0 +1,23 @@
+<form method="post" action="#">
+  <div>
+      <label for="nom">Nom&nbsp;:</label>
+      <input type="text" name="nom" />
+  </div>
+  <div>
+      <label for="prenom">Prenom&nbsp;:</label>
+      <input type="text" name="prenom" />
+  </div>
+  <div>
+      <label for="mail">Mail&nbsp;:</label>
+      <input type="text" name="nom" />
+  </div>
+  <div>
+      <label for="mdp">Mot de passe&nbsp;:</label>
+      <input type="text" name="nom" />
+  </div>
+  <div>
+      <input type="reset" value="effacer"/>
+      <input type="submit" value="envoyer"/>
+  </div>
+  <input type="hidden" name="frmRegistration"/>
+</form>
