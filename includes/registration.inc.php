0a493e169ffad16d34768ddd0102be2357f2fc10
Ludwig <leb.ludwig@gmail.com>
2018-04-05 13:38:25 +0200

    registration inscription

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,1 +1,61 @@
 <h1>Inscription</h1>
+<?php
+
+if (isset($_POST['frmRegistration'])) {
+    // Syntaxe classique
+    // if (isset($_POST['nom'])) {
+    //   $nom = $_POST['nom'];
+    // }
+
+    // else {
+    //   $nom = "";
+
+    // }
+    // Opérateur ternaire
+    // $nom = isset($_POST['nom']) ? $_POST['nom'] : "" ;       //operateur ternaire
+
+    // Opérateur NULL coalescent PHP7
+    $nom = $_POST['nom'] ?? "";
+    $prenom = $_POST['prenom'] ?? "";
+    $mail = $_POST['mail'] ?? "";
+    $mdp = $_POST['mdp'] ?? "";
+
+    $erreurs = array();
+
+    if($nom == "") array_push($erreurs, "Veuillez saisir votre nom");
+    if($prenom == "") array_push($erreurs, "Veuillez saisir votre prenom");
+    if($mail == "") array_push($erreurs, "Veuillez saisir votre mail");
+    if($mdp == "") array_push($erreurs, "Veuillez saisir votre mot de passe");
+
+    if (count($erreurs) > 0 ) {
+        $message = "<ul>";
+
+        foreach($erreurs as $ligneMessage) {
+         $message .= "<li>";
+         $message .= $ligneMessage;
+         $message .= "</li>" ;
+       }
+
+       $message .="<ul>";
+
+       echo $message;
+
+       include "frmRegistration.php";
+
+
+
+
+    }
+
+    else {
+      echo "Pas d'erreurs";
+
+    }
+}
+
+else{
+  echo "je ne viens pas du formulaire";
+  include "frmRegistration.php";
+}
+
+
