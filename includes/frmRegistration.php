437c7ffdc8c230fd7d3ebe64c689be8ba9d6f6eb
Ludwig <leb.ludwig@gmail.com>
2018-04-05 13:38:06 +0200

    form inscription

--- includes/frmRegistration.php
+++ includes/frmRegistration.php
@@ -1,20 +1,24 @@
 <form method="post" action="#">
   <div>
       <label for="nom">Nom&nbsp;:</label>
-      <input type="text" name="nom" />
+      <input type="text" name="nom" value="<?php if(isset($nom)) echo $nom; ?>"/>
   </div>
+  <br>
   <div>
       <label for="prenom">Prenom&nbsp;:</label>
-      <input type="text" name="prenom" />
+      <input type="text" name="prenom" value="<?php if(isset($prenom)) echo $prenom; ?>"/>
   </div>
+  <br>
   <div>
       <label for="mail">Mail&nbsp;:</label>
-      <input type="text" name="nom" />
+      <input type="text" name="mail" value="<?php if(isset($mail)) echo $mail; ?>"/>
   </div>
+  <br>
   <div>
       <label for="mdp">Mot de passe&nbsp;:</label>
-      <input type="text" name="nom" />
+      <input type="password" name="mdp" />
   </div>
+  <br>
   <div>
       <input type="reset" value="effacer"/>
       <input type="submit" value="envoyer"/>
